6807edf56918206ac5c4c957518577696c688df6
Reza Farajian <reza.farajian@gmail.com>
2014-10-17 15:08:24 -0700

    poker: create deck class

--- poker/lib/deck.rb
+++ poker/lib/deck.rb
@@ -1,2 +1,31 @@
+require_relative 'card'
+
 class Deck
+  attr_accessor :cards
+  
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
+  SUITS = [:clubs, :diamonds, :spades, :hearts]
+  
+  def initialize
+    @cards = []
+    build_deck
+  end
+  
+  def build_deck
+    VALUES.each do |value|
+      SUITS.each do |suit|
+        cards << Card.new(value, suit)
+      end
+    end
+  end
+  
+  def give_cards(n)
+    result = []
+    n.times { result << cards.shift }
+    result
+  end
+  
+  def accept_cards(card_arr)
+    card_arr.each { |card| cards << card }
+  end
 end
--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -1,4 +1,32 @@
 require_relative '../lib/deck'
 
 describe Deck do
+  let(:deck) { Deck.new }
+  
+  it "should have 52 cards" do
+    expect(deck.cards.count).to eq(52)
+  end
+  
+  it "should have no duplicate card" do
+    expect(deck.cards.uniq).to eq(deck.cards)
+  end
+  
+  describe "#give_cards" do
+    it "should be able to give cards from top of deck" do
+      expect(deck.give_cards(2).count).to eq(2)
+      expect(deck.cards.count).to eq(50)
+    end
+  end
+  
+  describe "#accept_cards" do
+    it "should be able to accept cards at bottom of deck" do
+      card1 = Card.new(6, :clubs)
+      card2 = Card.new(:king, :diamonds)
+      deck.accept_cards([card1, card2])
+      expect(deck.cards[-1]).to equal(card2)
+      expect(deck.cards[-2]).to equal(card1) 
+    end
+  end
+  
+  it "should be able to shuffle"
 end
