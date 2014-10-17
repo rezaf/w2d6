175dd89f7e960a86efa9b61082b588f222ce0fe8
Reza Farajian <reza.farajian@gmail.com>
2014-10-17 14:04:27 -0700

    finish towers, create my_transpose, start stock_picker methods and rspecc

--- lib/array_exercises.rb
+++ lib/array_exercises.rb
@@ -23,9 +23,58 @@ end
 
 class Towers
   
+  WINNING_COMBO = [3, 2, 1]
+  
   attr_accessor :towers
   
   def initialize
     @towers = [[3, 2, 1], [], []]
   end
+  
+  def user_input
+    puts "Tell us the tower to pick from:"
+    from = gets.chomp.to_i
+    puts "Tell us the tower to place this:"
+    to = gets.chomp.to_i
+    
+    [from, to]
+  end
+  
+  def move_disk(move_array)
+    towers[move_array[1]] << towers[move_array[0]].pop
+  end
+  
+  def play
+    move_disk(user_input)
+    
+  end
+  
+  def game_over?
+    towers[1] == WINNING_COMBO || towers[2] == WINNING_COMBO 
+  end
+end
+
+def my_transpose(arr)
+  transpose = Array.new(arr.count) { Array.new(arr.count) }
+  arr.each_with_index do |row, idx1|
+    row.each_with_index do |num, idx2|
+      transpose[idx2][idx1] = num
+    end
+  end
+  transpose
+end
+
+def stock_picker(prices)
+  answer = []
+  greatest_profit = 0
+  prices.each_with_index do |price, idx|
+    prices[(idx + 1)..-1].each_with_index do |price2, idx2|
+      if price2 - price > greatest_profit
+        answer = [idx, idx + idx2]
+        greatest_profit = price2 - price
+      end
+    end
+  end
+  p answer
+  answer
 end
--- spec/array_exercises_spec.rb
+++ spec/array_exercises_spec.rb
@@ -16,7 +16,7 @@ describe Array do
     end
   end
   
-  describe "two_sum" do
+  describe "#two_sum" do
     let(:arr1) { [-1, 0, 2, -2, 1] }
     it "should create a new array" do
       arr2 = arr1.two_sum
@@ -37,14 +37,58 @@ describe Towers do
     it "should init with 3 towers" do
       expect(game.towers.count).to eq(3)
     end
-    
+  
     it "tower 1 should init with 3 disks" do
       expect(game.towers[0].count).to eq(3)
     end
-    
-    it "should ask for user input"
-    it "should win if 3 disks are at a different tower"
-    it "should check to see if game is over"
+  end
+  
+  describe "move disk" do  
+  
+    it "should move disk" do
+      game.move_disk([0, 1])
+      expect(game.towers).to eq([[3, 2], [1], []])
+    end
+  
+  end
+
+  describe "game over?" do
+
+    it "should return that the game is over if the game is won" do
+      game.towers = [[], [], [3, 2, 1]]
+      expect(game.game_over?).to be_true
+    end
+  
+    it "should return that the game is not over if the game is not won" do
+      game.towers = [[], [3], [2, 1]]
+      expect(game.game_over?).to be_false
+    end
+  end  
+end
+
+describe "my_transpose" do
+  let(:output) { my_transpose([[0, 1, 2], [3, 4, 5], [6, 7, 8]]) }
+  
+  it "should return an array" do
+    expect(output).to be_a_kind_of(Array) 
+  end
+  
+  it "should return the transpose" do
+    expect(output).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
   end
 end
 
+describe "stock picker" do
+  let(:output) { stock_picker([5, 2, 3, 8, 7, 1, 9]) }
+  
+  it "should return an array" do
+    expect(output).to be_a_kind_of(Array) 
+  end
+  
+  it "should buy before selling" do
+    expect(output[0]).to be < output[1]
+  end
+  
+  it "should choose the most profitable buy and sell date"
+  
+end
