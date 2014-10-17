b6bab7a54a4749bf917e7311488d7e7cfef0261d
Reza Farajian <reza.farajian@gmail.com>
2014-10-17 10:36:31 -0700

    first commit

---
+++ lib/array_exercises.rb
@@ -0,0 +1,5 @@
+class Array
+  
+  def my_unique
+  end
+end
---
+++ spec/array_exercises_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/array_exercises'
+
+describe my_unique do
+  subject(:arr) { Array.new }
+  
+  it "should create an array"
+  it "should have no duplicate elements"
+end
+
